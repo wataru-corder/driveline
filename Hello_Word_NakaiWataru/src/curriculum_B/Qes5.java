2b4f3e634e539fea94ef02d6a1c91701f2ac464d
wataru <wataru.corder@gmail.com>
2025-07-28 22:11:30 +0900

    Qes5

--- Hello_Word_NakaiWataru/src/curriculum_B/Qes5.java
+++ Hello_Word_NakaiWataru/src/curriculum_B/Qes5.java
@@ -1,16 +1,16 @@
 package curriculum_B;
 
-public class Qes4 {
+public class Qes5 {
 
 	public static void main(String[] args) {
-		for(int i = 1; i <= 10; i++) {
+		for(int i = 1; i <= 9; i++) {
 			
-			for(int j = 1; j <= 10; j++) {
+			for(int j = 1; j <= 9; j++) {
 				int result = i * j;
 				if(j != 1) {
 					System.out.print(" || ");
 				}
-				System.out.printf("%03d * %03d = %03d", j, i, result);
+				System.out.printf("%02d * %02d = %02d", i, j, result);
 			}
 			 System.out.println();
 		}
