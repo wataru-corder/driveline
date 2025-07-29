4284a327e26de0e62e4d941bb3c8e5e07a899b80
wataru <wataru.corder@gmail.com>
2025-07-29 23:18:55 +0900

    19.クラスの基本 課題

--- Hello_Word_NakaiWataru/src/basicClass/Animal.java
+++ Hello_Word_NakaiWataru/src/basicClass/Animal.java
@@ -1,13 +1,21 @@
 package basicClass;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Animal {
 	
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		
+		Dog dog = new Dog("ねこ");
+		System.out.println(dog.name);
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		
+		Dog dog1 = new Dog(11);
+		System.out.println(dog1.count);
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		String formatterDate = now.format(formatter);
+		System.out.println(formatterDate);
 	}
 }
--- Hello_Word_NakaiWataru/src/basicClass/Dog.java
+++ Hello_Word_NakaiWataru/src/basicClass/Dog.java
@@ -2,11 +2,20 @@ package basicClass;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	
+	String name;
 	// Q2：フィールドに動物の数の変数を定義してください。
-	
+	int count;
+
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	
+	public Dog() {
+		this.name = "犬";
+	}
+	public Dog(String name) {
+		this.name = name;
+	}
+
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	
+	public Dog(int count) {
+		this.count = count;
+	}
 }
